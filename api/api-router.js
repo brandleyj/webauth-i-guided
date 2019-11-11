28381c1151bce91d88e6616d75ff35d831a6be9e
Justin Brandley <brandleyj@gmail.com>
2019-11-11 11:30:12 -0600

    added hash request

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
 
-router.get('/', (req, res) => {
-  res.json({ api: "It's alive" });
+router.get("/", (req, res) => {
+	res.json({ api: "It's alive" });
+});
+
+router.post("/hash", (req, res) => {
+	const { password } = req.body;
+	const hash = bcrypt.hashSync(password, 10);
+	res.status(201).json({ password: hash });
 });
 
 module.exports = router;
